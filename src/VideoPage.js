ea17db2e4d816078d788b98106158a360a819a48
matheus <matheus.forny@gmail.com>
2018-11-07 19:24:51 -0200

    Video em fullscreen ao iniciar

--- src/VideoPage.js
+++ src/VideoPage.js
@@ -4,9 +4,14 @@ import { Player } from 'video-react';
 // class component
 class SearchPage extends Component {
 
+    componentDidMount = function() {
+		this.refs.player.toggleFullscreen();
+	}
+
 	render() {
 		return (
 			<Player
+				ref="player"
 				autoPlay
 				onEnded={this.props.onEnded}
 				src={this.props.src}
